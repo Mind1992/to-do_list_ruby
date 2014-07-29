bfccf1caa20514b3c34098c2f2d46377e5c066af
Sergii Mertsalov <sergmerts@gmail.com>
2014-07-28 22:30:17 -0700

    add specs for list class

--- spec/to_do_spec.rb
+++ spec/to_do_spec.rb
@@ -1,5 +1,6 @@
 require 'rspec'
 require 'task'
+require 'list'
 
 describe Task do
   it 'us initialized with a description' do
@@ -15,5 +16,20 @@ describe Task do
   it 'lets you mark a task as completed' do
     test_task = Task.new('scrub the zebra')
     test_task.mark_complete.should eq true
+  end
+end
+
+
+describe List do 
+  it 'starts out with an empty list of tasks' do 
+    test_list = List.new("School stuff")
+    test_list.tasks.should eq []
+  end
+
+    it "can add tasks" do 
+    test_list = List.new("School stuff")
+    test_task = Task.new("Learn Ruby")
+    test_list.add_task(test_task)
+    test_list.tasks.should eq [test_task]
   end
 end
